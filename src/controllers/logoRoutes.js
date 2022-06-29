741b8ca28c31908bd31453309d97abd44681a3ba
Anne <92945218+vian-ai@users.noreply.github.com>
2022-06-29 11:54:53 -0600

    add delete and patch request to delete or update a logo by id

--- src/controllers/logoRoutes.js
+++ src/controllers/logoRoutes.js
@@ -12,4 +12,33 @@ router.get('/', (req, res) => {
         });
 });
 
+// create new logo data
+router.post('/', (req, res) => {
+    Logo.create(req.body)
+        .then((logo) => {
+            res.json(logo);
+        });
+});
+
+// delete logo data by id
+router.delete('/:id', (req, res) => {
+    Logo.findByIdAndDelete(req.params.id)
+        .then((logo) => {
+            res.json(logo);
+        });
+});
+
+// update logo data by id
+router.patch('/:id', (req, res) => {
+    const id = req.params.id;
+    const data = req.body;
+
+    Logo.findByIdAndUpdate(id, data, {
+        new: true
+    })
+        .then((logo) => {
+            res.json(logo);
+        });
+});
+
 module.exports = router;
