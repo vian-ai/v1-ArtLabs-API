12285fb9668457f046432f3dafe963ac3f1404ee
Anne <92945218+vian-ai@users.noreply.github.com>
2022-07-02 22:47:23 -0600

    host api

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/art_db', {
-    useNewUrlParser: true
-});
+let mongoURI = '';
+
+if (process.env.NODE_ENV === "production") {
+    mongoURI = process.env.DB_URL;
+} else {
+    mongoURI = "mongodb://localhost:27017/art_db";
+}
+
+mongoose.connect(mongoURI);
 
 module.exports = mongoose;
--- src/server.js
+++ src/server.js
@@ -3,29 +3,32 @@ require('dotenv').config();
 
 const express = require('express');
 const logger = require('morgan');
-const cors = require('cors');
 
 // importing routes
 const bannerRoute = require('./controllers/bannerRoutes');
 const emoteRoute = require('./controllers/emoteRoutes');
 const logoRoute = require('./controllers/logoRoutes');
 
-// define PORT
-const PORT = process.env.PORT;
+const cors = require('cors');
 
 // create express app
 const app = express();
 
 app.use(express.json());
-app.use(logger('dev'));
 app.use(cors());
+
+// logger middleware
+app.use(logger('dev'));
 
 // app routes
 app.use('/banners/', bannerRoute);
 app.use('/emotes/', emoteRoute);
 app.use('/logos/', logoRoute);
 
-// starting app on port
-app.listen(PORT, () => {
-    console.log(`Listening to port: ${PORT}`);
+// start app on port
+app.set('port', process.env.PORT || 8000);
+
+// starting app
+app.listen(app.get('port'), () => {
+    console.log(`Listening to port: ${app.get('port')}`);
 });
